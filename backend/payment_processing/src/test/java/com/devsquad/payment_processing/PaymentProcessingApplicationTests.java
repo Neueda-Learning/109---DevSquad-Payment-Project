97ad9e52bb4daa3bf84d8b0b3fdd32e10ec6ed5e
Bhavya Arora <bhavya56arora11@gmail.com>
2026-07-31 09:30:47 +0000

    New backend structure created

---
+++ backend/payment_processing/src/test/java/com/devsquad/payment_processing/PaymentProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsquad.payment_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
